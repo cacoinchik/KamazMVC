732a3e1979a63b292b4405c3c1c4888c0a889794
RK.Ibragimov <RK.Ibragimov@kamaz.ru>
2023-08-17 14:58:33 +0300

    Подготовлены контроллер и представление

---
+++ KamazMVC/Controllers/DocumentController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KamazMVC.Controllers
+{
+    public class DocumentController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
