6bd55fa25c449c1905eac8b509cad9f655eb37ca
RK.Ibragimov <RK.Ibragimov@kamaz.ru>
2023-08-17 14:03:30 +0300

    вывод небольшой инфы по аутентифицированному пользователю

--- KamazMVC/Controllers/ProfileController.cs
+++ KamazMVC/Controllers/ProfileController.cs
@@ -1,14 +1,25 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using KamazMVC.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace KamazMVC.Controllers
 {
     [Authorize]
     public class ProfileController : Controller
     {
+        private readonly AppDbContext _db;
+        public ProfileController(AppDbContext db)
+        {
+            _db = db;
+        }
         public async Task<IActionResult> Index()
         {
-            return View();
+            if(User.Identity.IsAuthenticated)
+            {
+                return View(await _db.Users.FirstOrDefaultAsync(user=>user.UserName==User.Identity.Name));
+            }
+            return BadRequest("Пользователь не найден, обратитесь в техническую поддержку");
         }
     }
 }
