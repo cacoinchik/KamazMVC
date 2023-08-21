c9d3dea7ebcec0e16af76d07df289dbff561fbb0
RK.Ibragimov <RK.Ibragimov@kamaz.ru>
2023-08-21 15:40:17 +0300

    Добавлено редактирование личных данных пользователя

--- KamazMVC/Controllers/ProfileController.cs
+++ KamazMVC/Controllers/ProfileController.cs
@@ -1,7 +1,11 @@
 ﻿using KamazMVC.Data;
+using KamazMVC.Models;
+using KamazMVC.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 
 namespace KamazMVC.Controllers
 {
@@ -9,9 +13,11 @@ namespace KamazMVC.Controllers
     public class ProfileController : Controller
     {
         private readonly AppDbContext _db;
-        public ProfileController(AppDbContext db)
+        private readonly UserManager<User> _userManager;
+        public ProfileController(AppDbContext db, UserManager<User> userManager)
         {
             _db = db;
+            _userManager = userManager;
         }
         public async Task<IActionResult> Index()
         {
@@ -20,6 +26,54 @@ namespace KamazMVC.Controllers
                 return View(await _db.Users.FirstOrDefaultAsync(user=>user.UserName==User.Identity.Name));
             }
             return BadRequest("Пользователь не найден, обратитесь в техническую поддержку");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditProfile(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            EditUserViewModel model = new EditUserViewModel
+            {
+                Id = user.Id,
+                Surname = user.Surname,
+                Name = user.Name,
+                Patronymic = user.Patronymic
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditProfile(EditUserViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                User user = await _userManager.FindByIdAsync(model.Id);
+                if(user != null)
+                {
+                    user.Surname = model.Surname;
+                    user.Name = model.Name;
+                    user.Patronymic = model.Patronymic;
+
+                    var result = await _userManager.UpdateAsync(user);
+
+                    if(result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Profile");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ KamazMVC/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KamazMVC.ViewModels
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "Ваша фамилия")]
+        public string? Surname { get; set; }
+
+        [Required]
+        [Display(Name = "Ваше имя")]
+        public string? Name { get; set; }
+
+        [Required]
+        [Display(Name = "Ваше отчество")]
+        public string? Patronymic { get; set; }
+    }
+}
