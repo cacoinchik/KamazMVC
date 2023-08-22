33c7ed9adf522b0ceea154884e1749caa936bee8
RK.Ibragimov <RK.Ibragimov@kamaz.ru>
2023-08-22 10:36:22 +0300

    Установлен и настроен пакет AspNetCoreRateLimit добавлено тестовое ограничение запросов

--- KamazMVC/Program.cs
+++ KamazMVC/Program.cs
@@ -1,3 +1,4 @@
+using AspNetCoreRateLimit;
 using KamazMVC.Data;
 using KamazMVC.Models;
 using Microsoft.AspNetCore.Identity;
@@ -7,9 +8,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
+//Подключение к БД
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
-builder.Services.AddIdentity<User,IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+//Подключение Identity
+builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
+
+//Подключение AspNetCoreRateLimit
+builder.Services.AddOptions();
+builder.Services.AddMemoryCache();
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+builder.Services.Configure<IpRateLimitPolicies>(builder.Configuration.GetSection("IpRatePolicies"));
+builder.Services.AddInMemoryRateLimiting();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+builder.Services.AddSingleton<IIpPolicyStore, DistributedCacheIpPolicyStore>();
+builder.Services.AddSingleton<IRateLimitCounterStore, DistributedCacheRateLimitCounterStore>();
+
 
 var app = builder.Build();
 
@@ -23,6 +37,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseIpRateLimiting();
 
 app.UseAuthorization();
 
