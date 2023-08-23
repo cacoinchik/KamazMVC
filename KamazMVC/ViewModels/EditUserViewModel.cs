d354659655d8ffaeb11b4abfe493e4bdbbdff864
RK.Ibragimov <RK.Ibragimov@kamaz.ru>
2023-08-23 14:55:08 +0300

    фикс изменения личных данных

--- KamazMVC/ViewModels/EditUserViewModel.cs
+++ KamazMVC/ViewModels/EditUserViewModel.cs
@@ -6,15 +6,12 @@ namespace KamazMVC.ViewModels
     {
         public string Id { get; set; }
 
-        [Required]
         [Display(Name = "Ваша фамилия")]
         public string? Surname { get; set; }
 
-        [Required]
         [Display(Name = "Ваше имя")]
         public string? Name { get; set; }
 
-        [Required]
         [Display(Name = "Ваше отчество")]
         public string? Patronymic { get; set; }
     }
