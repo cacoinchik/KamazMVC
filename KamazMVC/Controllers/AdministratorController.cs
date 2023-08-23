3fe64cbe080433d8f5c61f82144da84a7ad483d9
RK.Ibragimov <RK.Ibragimov@kamaz.ru>
2023-08-23 14:38:30 +0300

    Добавлена админка CRUD для ролей Добавлено назначение и удаление ролей ползователю

---
+++ KamazMVC/Controllers/AdministratorController.cs
@@ -0,0 +1,107 @@
+﻿using KamazMVC.Data;
+using KamazMVC.Models;
+using KamazMVC.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
+
+namespace KamazMVC.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class AdministratorController : Controller
+    {
+        private readonly AppDbContext _db;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<User> _userManager;
+        public AdministratorController(AppDbContext db, RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+        {
+            _db = db;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index() => View(await _roleManager.Roles.ToListAsync());
+
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpGet]
+        public async Task<IActionResult> UserList() => View(await _userManager.Users.ToListAsync());
+
+        [HttpPost]
+        public async Task<IActionResult> Create(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Administrator");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await _roleManager.DeleteAsync(role);
+            }
+            return RedirectToAction("Index", "Administrator");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var allRolles = await _roleManager.Roles.ToListAsync();
+                ChangeRoleViewModel model = new ChangeRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                    UserRoles = userRoles,
+                    AllRoles = allRolles
+                };
+                return View(model);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, List<string> roles)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var allRoles = await _roleManager.Roles.ToListAsync();
+                var addedRoles = roles.Except(userRoles);
+                var removedRoles = userRoles.Except(roles);
+
+                await _userManager.AddToRolesAsync(user, addedRoles);
+
+                await _userManager.RemoveFromRolesAsync(user, removedRoles);
+
+                return RedirectToAction("Index", "Administrator");
+            }
+            return NotFound();
+        }
+
+    }
+}
